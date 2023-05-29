a52c9934bf24fdc3e2a1ebbe9f9bef9310ab0349
Veronica <wmuriga@mit.edu>
2023-05-29 12:07:01 -0400

    using tensorboard for error calculations

--- late_computation_with_new_model.py
+++ late_computation_with_new_model.py
@@ -15,6 +15,9 @@ import sklearn.preprocessing
 # %matplotlib qt 
 import cv2
 import math
+import tensorflow as tf
+from tensorflow.keras.callbacks import Callback
+
 
 
 dataset_root_path = '/home/veronica/USANNIOMIT/sen2dwater_combined' # TODO: adjust path
@@ -43,69 +46,23 @@ NDDIval2 = datareader.generatorv2('NDDI',val_set,
                                         SHAPE,
                                         normalize=False)
 
-def calculate_mse(imageA, imageB):
-	# the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
-	# NOTE: the two images must have the same dimension
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
-	
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
-	return err
 
-def calculate_psnr(img1, img2):
-    # img1 and img2 have range [0, 255]
-    img1 = img1.astype(np.float64)
-    img2 = img2.astype(np.float64)
-    mse = np.mean((img1 - img2)**2)
-    if mse == 0:
-        return float('inf')
-    return 20 * math.log10(255.0 / math.sqrt(mse))
+# method 2 of calculating error metrics
+def error_metrics_using_tb(y_in, y_pr):
+  psnr, ssim, mse = [], [], []
+  for i in range(y_in.shape[0]):
+    x = tf.cast(y_in[i,...], tf.float32)
+    y = tf.cast(y_pr[i,...], tf.float32)
+    m1 = tf.reduce_mean(tf.image.psnr(x, y, 2.0))
+    m2 = tf.reduce_mean(tf.image.ssim(x, y, 2.0))
+    m3 = tf.reduce_mean(tf.keras.metrics.mean_squared_error(x, y))
 
-def ssim(img1, img2):
-    C1 = (0.01 * 255)**2
-    C2 = (0.03 * 255)**2
+    psnr.append(m1.numpy())
+    ssim.append(m2.numpy())
+    mse.append(m3.numpy())
 
-    img1 = img1.astype(np.float64)
-    img2 = img2.astype(np.float64)
-    kernel = cv2.getGaussianKernel(11, 1.5)
-    window = np.outer(kernel, kernel.transpose())
+  return psnr, ssim, mse 
 
-    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
-    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
-    mu1_sq = mu1**2
-    mu2_sq = mu2**2
-    mu1_mu2 = mu1 * mu2
-    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
-    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
-    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
-
-    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
-                                                            (sigma1_sq + sigma2_sq + C2))
-    return ssim_map.mean()
-
-
-def calculate_ssim(img1, img2):
-    '''calculate SSIM
-    the same outputs as MATLAB's
-    img1, img2: [0, 255]
-    '''
-    if not img1.shape == img2.shape:
-        raise ValueError('Input images must have the same dimensions.')  
-
-    if img1.ndim == 2:
-        return ssim(img1, img2)
-    elif img1.ndim == 3:
-        if img1.shape[2] == 3:
-            ssims = []
-            for i in range(3):
-                ssims.append(ssim(img1, img2))
-            return np.array(ssims).mean()
-        elif img1.shape[2] == 1:
-            return ssim(np.squeeze(img1), np.squeeze(img2))
-    else:
-        raise ValueError('Wrong input image dimensions.')
 
 # our trained ndwi model
 # ndwi_model = load_model('/content/drive/MyDrive/results-lenovo/ndwi_model.h5')
@@ -136,19 +93,6 @@ def getNDDI(NDVI,NDWI):
 
 nddi_from_NDVI_NDWI = []
 
-# save error metrics
-ndvi_mse_arr = []
-ndwi_mse_arr = []
-nddi_mse_arr = []
-nddi_from_ndvi_ndwi_mse_arr = []
-ndvi_ssim_arr = []
-ndwi_ssim_arr = []
-nddi_ssim_arr = []
-nddi_from_ndvi_ndwi_ssim_arr = []
-ndvi_psnr_arr = []
-ndwi_psnr_arr = []
-nddi_psnr_arr = []
-nddi_from_ndvi_ndwi_psnr_arr = []
 
 print(min(ndvi_prediction.flatten()), max(ndvi_prediction.flatten()))
 
@@ -157,54 +101,15 @@ for i in range(shape[0]): # get NDDI from NDWI and NDVI
   ndwi = ndwi_prediction[i] 
   nddi_from_NDVI_NDWI.append(getNDDI(ndvi,ndwi))
 
+# method 2 of error calculations
+ndvi_psnr_arr, ndvi_ssim_arr, ndvi_mse_arr = error_metrics_using_tb(NDVI_gt, ndvi_prediction)
+ndwi_psnr_arr, ndwi_ssim_arr, ndwi_mse_arr = error_metrics_using_tb(NDWI_gt, ndwi_prediction)
+nddi_psnr_arr, nddi_ssim_arr, nddi_mse_arr = error_metrics_using_tb(NDDI_gt, nddi_prediction)
+nddi_from_ndvi_ndwi_psnr_arr, nddi_from_ndvi_ndwi_ssim_arr, nddi_from_ndvi_ndwi_mse_arr = error_metrics_using_tb(NDDI_gt, np.array(nddi_from_NDVI_NDWI))
 
-  # get error metrics
-  ndvi_mse = calculate_mse(ndvi_prediction[i], NDVI_gt[i])
-  ndwi_mse = calculate_mse(ndwi_prediction[i], NDWI_gt[i])
-  nddi_mse = calculate_mse(nddi_prediction[i], NDDI_gt[i])
-  nddi_from_ndvi_ndwi_mse = calculate_mse(nddi_prediction[i], nddi_from_NDVI_NDWI[i])
-
-  ndvi_ssim = calculate_ssim(ndvi_prediction[i], NDVI_gt[i])
-  ndwi_ssim = calculate_ssim(ndwi_prediction[i], NDWI_gt[i])
-  nddi_ssim = calculate_ssim(nddi_prediction[i], NDDI_gt[i])
-  nddi_from_ndvi_ndwi_ssim = calculate_ssim(nddi_prediction[i], nddi_from_NDVI_NDWI[i])
-
-
-  ndvi_psnr = calculate_psnr(ndvi_prediction[i], NDVI_gt[i])
-  ndwi_psnr = calculate_psnr(ndwi_prediction[i], NDWI_gt[i])
-  nddi_psnr = calculate_psnr(nddi_prediction[i], NDDI_gt[i])
-  nddi_from_ndvi_ndwi_psnr = calculate_psnr(nddi_prediction[i], nddi_from_NDVI_NDWI[i])
-
-
-
-  # print("ndvi_mse: ", ndvi_mse)
-  # print("ndwi_mse: ", ndwi_mse)
-  # print("nddi_mse: ", nddi_mse)
-  # print("nddi_from_ndvi_ndwi_mse: ", nddi_from_ndvi_ndwi_mse)
-  # print("ndvi_ssim :", ndvi_ssim)
-  # print("ndwi_ssim: ", ndwi_ssim)
-  # print("nddi_ssim: ", nddi_ssim)
-  # print("nddi_from_ndvi_ndwi_ssim :", nddi_from_ndvi_ndwi_ssim)
-  # print("ndvi_psnr: ", ndvi_psnr)
-  # print("ndwi_psnr: ", ndwi_psnr)
-  # print("nddi_psnr: ", nddi_psnr)
-  # print("nddi_from_ndvi_ndwi_psnr: ", nddi_from_ndvi_ndwi_psnr)
-
-  ndvi_mse_arr.append(ndvi_mse)
-  ndwi_mse_arr.append(ndwi_mse)
-  nddi_mse_arr.append(nddi_mse)
-  nddi_from_ndvi_ndwi_mse_arr.append(nddi_from_ndvi_ndwi_mse)
-  ndvi_ssim_arr.append(ndvi_ssim)
-  ndwi_ssim_arr.append(ndwi_ssim)
-  nddi_ssim_arr.append(nddi_ssim)
-  nddi_from_ndvi_ndwi_ssim_arr.append(nddi_from_ndvi_ndwi_ssim)
-  ndvi_psnr_arr.append(ndvi_psnr)
-  ndwi_psnr_arr.append(ndwi_psnr)
-  nddi_psnr_arr.append(nddi_psnr)
-  nddi_from_ndvi_ndwi_psnr_arr.append(nddi_from_ndvi_ndwi_psnr)
 
 print("ndvi_mse: ", np.mean(ndvi_mse_arr))
-print("ndwi_mse: ", np.mean(ndwi_mse))
+print("ndwi_mse: ", np.mean(ndwi_mse_arr))
 print("nddi_mse: ", np.mean(nddi_mse_arr))
 print("nddi_from_ndvi_ndwi_mse: ", np.mean(nddi_from_ndvi_ndwi_mse_arr))
 print("ndvi_ssim :", np.mean(ndvi_ssim_arr))
@@ -215,22 +120,8 @@ print("ndvi_psnr: ", np.mean(ndvi_psnr_arr))
 print("ndwi_psnr: ", np.mean(ndwi_psnr_arr))
 print("nddi_psnr: ", np.mean(nddi_psnr_arr))
 print("nddi_from_ndvi_ndwi_psnr: ", np.mean(nddi_from_ndvi_ndwi_psnr_arr))
-print("ndvi_psnr: ", ndvi_psnr_arr)
-print("ndwi_psnr: ", ndwi_psnr_arr)
-print("nddi_psnr: ", nddi_psnr_arr)
-print("nddi_from_ndvi_ndwi_psnr: ", nddi_from_ndvi_ndwi_psnr_arr)
-
-
-# NDDItheoreticGT = getNDDI(NDVI_gt,NDWI_gt)
 
 fig, axes = plt.subplots(nrows = 2, ncols = BATCH_SIZE, figsize = (BATCH_SIZE*5,3*5))
-# font = {'family' : 'normal',
-#         'weight' : 'bold',
-#         'size'   : 22}
-
-# matplotlib.rc('font', **font)
-
-# plt.rcParams["font.family"] = "sans-serif"
 plt.rcParams["font.size"] = "8"
 
 for i in range(BATCH_SIZE):
@@ -274,7 +165,6 @@ for i in range(BATCH_SIZE):
   axes[1,i].imshow(nddi_from_NDVI_NDWI[i])
   axes[2,i].imshow(nddi_prediction[i,:,:,0])
 
-
   # axes[0,i].set_title('GT')
   # axes[1,i].set_title('Computed')
   # axes[2,i].set_title('Predicted')
@@ -287,5 +177,3 @@ for i in range(BATCH_SIZE):
 plt.savefig('img/nddi.png', transparent = True)
 # plt.show()
 # plt.close()
-
-
